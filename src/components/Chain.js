4c9dc1b1e75331ce5c270772e84b077747422187
Tim <virtualxself@gmail.com>
2022-12-08 17:18:27 +0200

    add block height display and timers for all displays

--- src/components/Chain.js
+++ src/components/Chain.js
@@ -12,13 +12,14 @@ function Chain(props) {
   const chainId = chain.chain;
   const chainApi = new Blockchain(chain.api[0]);
   const [setCurrentChain] = useOutletContext();
+  const [blockHeight, setBlockHeight] = useState('');
   const [circulatingSupply, setCirculatingSupply] = useState('');
   const [totalBonded, setTotalBonded] = useState('');
 
   // ОБНОВЛЯЕМ ТЕКУЩУЮ СЕТЬ 
   // Примечание: нужно для корректного отображения сети в выпадающем меню хедера в том случае, когда переход
   // на страницу сети осуществлён не "пошагово" с главной, а вводом полного пути в адресную строку браузера.
-  React.useEffect(() => {
+  useEffect(() => {
     setCurrentChain(chain)
   }, [])
 
@@ -31,18 +32,48 @@ function Chain(props) {
   // делится. Из-за этого полученные значения приходится обрабатывать по-разному, дополнительно обрезая монеты в обороте.
 
   // ПОЛУЧАЕМ ВСЕ МОНЕТЫ В ОБОРОТЕ
-  useEffect(() => {
+  const setCirculatingTokens = () => {
     chainApi.getCirculatingSupply()
       .then(result => {
         const cutted = result.slice(0, -(chain.decimals + 1)); // обрезаем строку на лишние нули плюс символ точки
         setCirculatingSupply(cutted);
       })
-  }, [chain])
+  }
 
   // ПОЛУЧАЕМ СУММУ ВСЕХ ЗАСТЕЙКАННЫХ МОНЕТ
-  useEffect(() => {
+  const setStakedTokens = () => {
     chainApi.getBondedTokens()
-      .then(result => setTotalBonded(result))
+      .then(result => setTotalBonded(result));
+  }
+
+  // ПОЛУЧАЕМ ПОСЛЕДНИЙ БЛОК
+  const setLatestBlock = () => {
+    chainApi.getLatestBlock()
+      .then(result => {
+        setBlockHeight(result.block.last_commit.height)
+      })
+  };
+
+  // ОБНОВЛЯЕМ ДАННЫЕ 
+  // Примечание: return в конце хука необходим для того, чтобы выполнить некий код при размонтировании компонента.
+  // В данном случае он сбрасывает мои таймеры - без этого при переключении между разными сетями дисплей с данными
+  // начинали лагать, показывая попеременно информацию то из одной, то из другой сети. Как я понял, это происходило
+  // потому, что если таймер не сбросить, то он сохраняет используемое им лексическое окружение, и простое переключение
+  // между сетями/компонентами тут не поможет - оттуда и глюки. Странная тема, но интересная - буду знать.
+  useEffect(() => {
+    setLatestBlock();
+    setStakedTokens();
+    setCirculatingTokens();
+    let latestBlockTimer = setInterval(setLatestBlock, 2000); // 2 сек.
+    let stakedTokensTimer = setInterval(setStakedTokens, 10000); // 10 сек.
+    let circulatingTokensTimer = setInterval(setCirculatingTokens, 300000); // 5 мин.
+    // Примечание: токены в обороте обновляются так редко (если я правильно думаю, раз в сутки), что тут даже таймер
+    // не обязателен, но я его добавил скорее ради общей структуры и красоты кода, нежели из реальной нужды.
+    return () => {
+      clearTimeout(latestBlockTimer);
+      clearTimeout(stakedTokensTimer);
+      clearTimeout(circulatingTokensTimer);
+    };
   }, [chain])
 
   return (
@@ -50,6 +81,10 @@ function Chain(props) {
       <h1 className="chain__heading">{chainName}</h1>
       <span className="chain__subheading">{chainId}</span>
       <div className="chain__common-info">
+        <div className="chain__info">
+          <span className="chain__info-heading">Current Block Height:</span>
+          <span className="chain__info-data">{Number(blockHeight).toLocaleString('en')}</span>
+        </div>
         <div className="chain__info">
           <span className="chain__info-heading">Bonded Tokens:</span>
           <span className="chain__info-data">{getTweakedTokens(totalBonded, chain.decimals).toLocaleString('en')}<span>{chain.symbol}</span></span>
